3e0b965f128d29493ada98d238ec7400267111a2
Plasma Intec <plasmaintec@i.local>
2019-09-16 18:48:09 -0700

    Use pickle to store retrieved reviews

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from aliexpress import get_reviews
 from opticats import write_reviews
+import pickle
 
-reviews = get_reviews()
+try:
+	reviews = pickle.load(open('save.p', 'rb'))
+except:
+	reviews = get_reviews()
+pickle.dump(reviews, open('save.p', 'wb'))
 write_reviews(reviews)
